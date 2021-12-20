bebba81d598ef020844442fab456b246b80d8efd
gurkirat <gurkiratsinghofficial@gmail.com>
2021-12-20 21:23:58 +0530

    empty commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,8 +16,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "Gmail",
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
+    user: process.env.EMAIL, 
+    pass: process.env.PASSWORD, 
   },
 });
 
